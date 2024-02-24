simutrans/simutrans

commit 3c457b79a86a7ea8f2c34778fd5d92c17288e405

FIX: (THLeaderH) crossings upgrade in ways are upgraded
git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11016 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/obj/way/weg.cc
@@ -266,6 +266,7 @@ void weg_t::count_sign()
 	flags &= ~(HAS_SIGN|HAS_SIGNAL|HAS_CROSSING);
 	const grund_t *gr=welt->lookup(get_pos());
 	if(gr) {
+		max_speed = desc->get_topspeed(); // reset max_speed
 		uint8 i = 1;
 		// if there is a crossing, the start index is at least three ...
 		if(  gr->ist_uebergang()  ) {

src/simutrans/builder/wegbauer.cc
@@ -2617,8 +2617,7 @@ void way_builder_t::build_road()
 				weg->set_gehweg(add_sidewalk);
 				player_t::add_maintenance( player_builder, weg->get_desc()->get_maintenance(), weg->get_desc()->get_finance_waytype());
 				weg->set_owner(player_builder);
-				// respect speed limit of crossing
-				weg->count_sign();
+				upgrade_crossing_if_needed(gr);
 			}
 		}
 		else {
@@ -2712,6 +2711,7 @@ void way_builder_t::build_track()
 					weg->set_owner(player_builder);
 					// respect speed limit of crossing
 					weg->count_sign();
+					upgrade_crossing_if_needed(gr);
 				}
 			}
 			else {
@@ -3073,4 +3073,15 @@ uint32 way_builder_t::calc_distance( const koord3d &pos, const koord3d &mini, co
 		dist += (pos.z - maxi.z) * s.way_count_slope;
 	}
 	return dist;
+}
+
+
+void way_builder_t::upgrade_crossing_if_needed( const grund_t* gr )
+{
+	crossing_t* crossing = gr->find<crossing_t>();
+	if(  !gr->ist_uebergang()  ||  !crossing  ) {
+		// A crossing does not exist on the given tile.
+		return;
+	}
+	crossing->finish_rd();
 }

src/simutrans/builder/wegbauer.h
@@ -173,6 +173,8 @@ private:
 	void build_powerline();
 	void build_river();
 
+	void upgrade_crossing_if_needed(const grund_t*);
+
 	uint32 calc_distance( const koord3d &pos, const koord3d &mini, const koord3d &maxi );
 
 public:

tests/tests/test_way_road.nut
@@ -758,6 +758,23 @@ function test_way_road_upgrade_crossing()
 	// clean up
 	ASSERT_EQUAL(remover.work(pl, coord3d(3, 1, 0), coord3d(3, 3, 0), "" + wt_rail), null)
 	ASSERT_EQUAL(remover.work(pl, coord3d(2, 2, 0), coord3d(4, 2, 0), "" + wt_road), null)
+
+	// upgrade rail with crossing; the crossing is upgraded
+	{
+		local fast_rail = way_desc_x("wooden_sleeper_track")
+		ASSERT_EQUAL(command_x.build_way(pl, coord3d(2, 2, 0), coord3d(4, 2, 0), slow_road, true), null)
+		// The crossing whose speed limit for rail is 80kph is built here.
+		ASSERT_EQUAL(command_x.build_way(pl, coord3d(3, 1, 0), coord3d(3, 3, 0), rail, true), null)
+		// upgrade rail. The crossing should be upgraded to the one whose speed limit for rail is 160kph.
+		ASSERT_EQUAL(command_x.build_way(pl, coord3d(3, 1, 0), coord3d(3, 3, 0), fast_rail, true), null)
+
+		ASSERT_EQUAL(tile_x(3, 2, 0).get_way(wt_road).get_max_speed(), min(crossing_road_speed, slow_road.get_topspeed()))
+		ASSERT_EQUAL(tile_x(3, 2, 0).get_way(wt_rail).get_max_speed(), min(crossing_rail_speed, fast_rail.get_topspeed()))
+	}
+
+	// clean up
+	ASSERT_EQUAL(remover.work(pl, coord3d(3, 1, 0), coord3d(3, 3, 0), "" + wt_rail), null)
+	ASSERT_EQUAL(remover.work(pl, coord3d(2, 2, 0), coord3d(4, 2, 0), "" + wt_road), null)
 	RESET_ALL_PLAYER_FUNDS()
 }
 

commit 55c6769ce8ec29bcfeaf9793a4a4cf78cfc2efa4

FIX: Wrong speed of bridge ramps in some cases
Bug was introduced in r11016.
Cf. https://www.simutrans-forum.de/mybb/showthread.php?tid=9668

git-svn-id: svn://tron.homeunix.org/simutrans/simutrans/trunk@11077 8aca7d54-2c30-db11-9de9-000461428c89

src/simutrans/obj/way/weg.cc
@@ -285,17 +285,15 @@ void weg_t::count_sign()
 	flags &= ~(HAS_SIGN|HAS_SIGNAL|HAS_CROSSING);
 	const grund_t *gr=welt->lookup(get_pos());
 	if(gr) {
-		max_speed = desc->get_topspeed(); // reset max_speed
 		uint8 i = 1;
 		// if there is a crossing, the start index is at least three ...
 		if(  gr->ist_uebergang()  ) {
+			max_speed = desc->get_topspeed(); // reset max_speed
 			flags |= HAS_CROSSING;
 			i = 3;
 			const crossing_t* cr = gr->find<crossing_t>();
-			uint32 top_speed = cr->get_desc()->get_maxspeed( cr->get_desc()->get_waytype(0)==get_waytype() ? 0 : 1);
-			if(  top_speed < max_speed  ) {
-				max_speed = top_speed;
-			}
+			const sint32 top_speed = cr->get_desc()->get_maxspeed( cr->get_desc()->get_waytype(0)==get_waytype() ? 0 : 1);
+			max_speed = min(max_speed, top_speed);
 		}
 		// since way 0 is at least present here ...
 		for( ;  i<gr->get_top();  i++  ) {