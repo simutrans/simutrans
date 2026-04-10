111f667c96b0bbfb5a78ef032544651572c2a249
ceeac <git.ceeac@gmail.com>
2026-04-10 17:25:34 +0000

    Remove unused function
    git-svn-id: svn://localhost/simutrans/trunk@11925 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/obj/gebaeude.h
+++ src/simutrans/obj/gebaeude.h
@@ -164,9 +164,6 @@ public:
 	 */
 	void finish_rd() OVERRIDE;
 
-	// currently animated
-	bool is_sync() const { return sync; }
-
 	/**
 	 * @returns pointer to first tile of a multi-tile building.
 	 */
