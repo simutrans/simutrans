ecca332dca25025833f5ce6641491f8886a3ef9f
Markus Pristovsek <prissi@physik.tu-berlin.de>
2026-09-06 06:54:30 +0000

    Added assert to find orphaned buildings with x==0 or z==0 or more thasn 250 tiles from town cetre
    git-svn-id: svn://localhost/simutrans/trunk@12261 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/world/simcity.cc
+++ src/simutrans/world/simcity.cc
@@ -614,6 +614,8 @@ void stadt_t::add_gebaeude_to_stadt(const gebaeude_t* gb, bool ordered)
 		for(grund_t* gr : gb_tiles ) {
 			gebaeude_t* add_gb = gr->find<gebaeude_t>();
 			assert(!buildings.is_contained(add_gb));
+			assert(add_gb->get_pos() == gr->get_pos());
+			assert(add_gb->get_pos().x != 0  && add_gb->get_pos().y != 0  &&  koord_distance(add_gb->get_pos(), get_pos()) < 250);
 			if( ordered ) {
 				buildings.insert_ordered( add_gb, level, compare_gebaeude_pos );
 			}
