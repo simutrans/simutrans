dc3e111c22db86cc60f984246b182f5fc6906dbf
Markus Pristovsek <prissi@physik.tu-berlin.de>
2026-09-22 06:55:57 +0000

    FIX: restore forbidden rule of old games for any player
    git-svn-id: svn://localhost/simutrans/trunk@12292 8aca7d54-2c30-db11-9de9-000461428c89

--- src/simutrans/dataobj/scenario.cc
+++ src/simutrans/dataobj/scenario.cc
@@ -1161,19 +1161,23 @@ void scenario_t::rdwr(loadsave_t *file)
 		}
 	}
 	else {
-		// only restore rules for player 1 in old games
-		uint32 count = forbidden_tools[1].get_count();
+		// only restore rules for player 0 in old games and copy
+		uint32 count = forbidden_tools[0].get_count();
 		file->rdwr_long(count);
 
 		for (uint32 i = 0; i < count; i++) {
 			if (file->is_loading()) {
-				forbidden_tools[1].append(new forbidden_t());
+				forbidden_tools[0].append(new forbidden_t());
 			}
 			forbidden_tools[1][i]->rdwr(file);
 		}
-		// missing: copy them to every player
+		// copy them to every player
+		for (uint pnr = 0; pnr < MAX_PLAYER_COUNT; pnr++) {
+			for (uint32 i = 2; i < count; i++) {
+				forbidden_tools[pnr].append(forbidden_tools[0][i]);
+			}
+		}
 	}
-
 	// cached strings
 	if (file->is_version_atleast(120, 3)) {
 		dynamic_string::rdwr_cache(file);
